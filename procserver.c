3637ae8132fd3e5ff3e3f7c2fc2537b34883665b
Olli Hollmen <olli.hollmen@gmail.com>
2020-01-18 08:04:34 -0800

    2019-02-24 Latest changes for POST parsing and submission

--- procserver.c
+++ procserver.c
@@ -74,6 +74,12 @@ static int send_page (struct MHD_Connection *connection,  const char* page, int 
   MHD_destroy_response (response);
   return ret;
 }
+
+#define con_info_need_mem(con_info, cnt) (con_info->used + cnt + 1)
+
+/** Destroy POST Request info freeing all allocated memory.
+* It is recommended that this is called in 
+*/
 void con_info_destroy(CONNINFO *con_info) { free(con_info->postdata); free(con_info->conttype); free(con_info); return; }
 /** Allocate */
 CONNINFO *con_info_create(struct MHD_Connection *connection) {
@@ -81,7 +87,7 @@ CONNINFO *con_info_create(struct MHD_Connection *connection) {
   const char * clen = MHD_lookup_connection_value (connection, MHD_HEADER_KIND, "Content-length");
   fprintf(stderr, "create(): CT: %s, CLEN: %s\n", ct, clen);
   
-  int initsize = 128;
+  int initsize = 128; // Content buffer con_info.postdata
   
   // TODO: Possibly Mark content-type to con_info
   CONNINFO * con_info = calloc (1, sizeof (CONNINFO));
@@ -89,6 +95,8 @@ CONNINFO *con_info_create(struct MHD_Connection *connection) {
   // NEW:
   con_info->contlen  = atoi(clen);
   con_info->conttype = strdup(ct);
+  // Allow contlen to dictate initial size. Reallocations should not occur w. this.
+  if (con_info->contlen) { initsize = con_info_need_mem(con_info, con_info->contlen); }
   // Test against post_body_max (from where ?)
   if (con_info->contlen > 8000) { con_info_destroy(con_info); return NULL; }
   con_info->postdata = (char*)calloc(initsize, sizeof(char)); // TODO: Decide on initial size (config?)
@@ -99,7 +107,7 @@ CONNINFO *con_info_create(struct MHD_Connection *connection) {
   return con_info;
 }
 
-#define con_info_need_mem(con_info, cnt) (con_info->used + cnt + 1)
+
 
 /** Parse POST Body incrementally.
 * This gets called multiple times as a result of MHD answer_to_connection() being called multiple times with request types
@@ -108,14 +116,17 @@ CONNINFO *con_info_create(struct MHD_Connection *connection) {
 * - INITED (0) con_info has been allocated and returned via param list con_cls
 * - PARSING (1) the fragments being passed by calls to answer_to_connection() are being parsed.
 * - COMPLETE (2) when body parsing has been completed.
-* @return MHD_NO for fatal errors that should terminate whole HTTP request, MHD_YES for valid state an progression of parsing.
+* @return MHD_NO for fatal errors that should terminate whole HTTP request, MHD_YES for valid state and progression of parsing.
 * A typical usage within answer_to_connection() might look like this:
      ...
 */
 int post_body_parse(struct MHD_Connection *connection, const char *upload_data,  size_t *upload_data_size, void ** con_cls) {
   if (!connection) { return MHD_NO; }
   
-  CONNINFO *con_info = NULL;
+  CONNINFO * con_info = NULL;
+  con_info = *con_cls;
+  // Enable following if-else blocks to keep more of the parsing state handling here (instead of macros)
+  /*
   // Establishing con_cls (last param of request handler (answer_to_connection()))
   if (!*con_cls) {
     con_info = con_info_create(connection);
@@ -123,57 +134,103 @@ int post_body_parse(struct MHD_Connection *connection, const char *upload_data, 
     *con_cls = con_info;
   }
   else { con_info = *con_cls; }
+  
   ///////////// Transition to parsing and getting (first fragment of) body content /////
   if (!con_info->is_parsing) { con_info->is_parsing = 1; return MHD_YES; } // 1
+  */
   // POST Body Chunk/fragment coming in OR all fragments parsed
-  // else { // else is redundant
-    // New chunk coming
-    if (*upload_data_size) {
-      int cnt = *upload_data_size;
-      int need = con_info_need_mem(con_info, cnt); // (con_info->used + cnt + 1);
-      if (need >= con_info->size) {
-        int newsize = 2*con_info->size; // 2X may not be enough (max(newsize, need))
-	if (need > newsize) { newsize = need; }
-        con_info->postdata = realloc(con_info->postdata, newsize);
-	if (!con_info->postdata) { return MHD_NO; }
-	if (con_info->debug) { fprintf(stderr, "Realloc from %d to: %d\n", con_info->size, newsize); }
-	con_info->size = newsize;
-	
-      }
-      // Receive the post data and write them into the bufffer
-      strncat(con_info->postdata, upload_data, cnt); // string(upload_data, *upload_data_size);
-      con_info->used += cnt;
-      *upload_data_size = 0; // Mark/flag processed / consumed ?
-      return MHD_YES;
+  // 
+  if (!con_info) { return MHD_NO; } // Should always have con_info by now
+  
+  // New chunk coming
+  if (*upload_data_size) {
+    int cnt = *upload_data_size;
+    int need = con_info_need_mem(con_info, cnt); // (con_info->used + cnt + 1);
+    if (need > con_info->size) { // Fixed (off-by-one) >= to > to not realloc unnecessarily
+      int newsize = 2*con_info->size; // Advisory, 2X may not be enough (Need: MAX(newsize, need))
+      if (need > newsize) { newsize = need; }
+      con_info->postdata = (char*)realloc(con_info->postdata, newsize);
+      if (!con_info->postdata) { return MHD_NO; }
+      con_info->debug && fprintf(stderr, "Realloc from %d to: %d\n", con_info->size, newsize);
+      con_info->size = newsize;
+
     }
-    else { // Done (*upload_data_size is 0)
-      ///////// Data now in con_info->postdata //////////
-      if (con_info->debug) { fprintf(stderr, "Got POST Body:\n%s\n", con_info->postdata); }
-      // *con_cls = NULL; // Mark/flag "Done" ? Set outside because there's no access to con_info w/o *con_cls !
-      con_info->is_parsing = 2;
-      // return 1;
-    }
-  // }
+    // Receive the post data and write them into the bufffer (new: binary append)
+    // strncat(con_info->postdata, upload_data, cnt); // string(upload_data, *upload_data_size); // OLD (for text content only)
+    memcpy(con_info->postdata + con_info->used, upload_data, cnt); // Tolerates binary and does not need to find end.
+    con_info->used += cnt;
+    con_info->postdata[con_info->used] = '\0'; // ->used gets us lastusedoffset + 1
+    *upload_data_size = 0; // Mark/flag processed / consumed (Per MHD doc)
+  }
+  else { // Done (*upload_data_size is 0, last call)
+    ///////// Data now in con_info->postdata //////////
+    con_info->debug && fprintf(stderr, "Got POST Body:\n%s\n", con_info->postdata);
+    // *con_cls = NULL; // Mark/flag "Done" ? Set outside because there's no access to con_info w/o *con_cls !
+    con_info->is_parsing = 2;
+  }
+  
   return MHD_YES; // 1;
 }
-
-/** Generic POST Body parsing.
-* See: MHD_post_process and MHD_create_post_processor
-MHD_ContentReaderFreeCallback
-* MHD_RequestCompletedCallback set by  MHD_OPTION_NOTIFY_COMPLETED (2 pointer params)
+/** Macros for detecting parsing state (from MHD multiple calls to response handler).
+* Macros have to be called in the order they appear here (See also example for post_body_parse()).
+* This is because they also change the state of the parsing.
+* These macros eliminate the calls to parser.
+* @todo: Name parsing_* instead of parse_* (?)
 */
+// Usage: if (parse_no_state(c)) { return MHD_NO; }
+// Do note call parse function before this detection
+#define parse_no_state(c) (!c)
+// Usage: if (parse_starting(c)) { return MHD_YES; }
+// Set *con_cls and Also transition 0 -> 1
+#define parse_starting(c) ((!*con_cls) && (!c->is_parsing) && (c->is_parsing = 1) && (*con_cls = c))
+// Usage: if (parse_running(c))  { return MHD_YES; }
+// Keep state, no transitions
+#define parse_running(c)  ((*con_cls) && (c->is_parsing == 1))
+// Usage: parse_ending(c) . TODO: consider if we should stick with setting state (2) inside POST parser (NOT macro)
+#define parse_ending(c)   ((*con_cls) && (!*upload_data_size) && (c->is_parsing) && (c->is_parsing = 2)) // COMPLETE
+
 int answer_to_connection (void *cls, struct MHD_Connection *connection,
     const char *url, const char *method, const char *version,
     const char *upload_data, size_t *upload_data_size, void **con_cls) {
   if (!strcmp (method, "GET")) {return send_page(connection, "No GET allowed !!!\n", MHD_HTTP_OK);}
-  // Must be POST or PUT
+  // Must be POST or PUT w. Body
   
-  printf("URL(%s): %s Datasize: %lu\n", method, url, *upload_data_size);
-  // TODO: CONNINFO *con_info = *con_cls ? (CONNINFO *)*con_cls : ...create(connection);
-  // TODO: if (start_parse()) {} // Also transition 0 -> 1 (!c->is_parsing) && (c->is_parsing = 1) // Name parsing_starting
+  printf("URL(%s): %s Datasize: %lu, con_cls: %p\n", method, url, *upload_data_size, *con_cls);
+  
+  CONNINFO *con_info = *con_cls ? (CONNINFO *)*con_cls : con_info_create(connection);
+  if (parse_no_state(con_info)) { return MHD_NO; }
+  if (parse_starting(con_info)) { return MHD_YES; }
+  int pret = post_body_parse(connection, upload_data, upload_data_size, con_cls);
+  if (!pret) { return MHD_NO; } // Fatal error - terminate request
+  if (parse_running(con_info))  { return MHD_YES; }
+  parse_ending(con_info);
+  ////////// Send the response the most ordinary way /////////////
+  struct MHD_Response *
+    response = MHD_create_response_from_buffer(strlen(con_info->postdata), (void*)con_info->postdata, MHD_RESPMEM_MUST_COPY);
+  MHD_add_response_header(response, MHD_HTTP_HEADER_CONTENT_TYPE, "application/json");
+  int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+  MHD_destroy_response(response);
+
+  return ret;
+}
+
+/** Generic POST Body parsing.
+* See: MHD_post_process and MHD_create_post_processor
+* MHD_ContentReaderFreeCallback
+* MHD_RequestCompletedCallback set by  MHD_OPTION_NOTIFY_COMPLETED (2 pointer params)
+*/
+int answer_to_connection1 (void *cls, struct MHD_Connection *connection,
+    const char *url, const char *method, const char *version,
+    const char *upload_data, size_t *upload_data_size, void **con_cls) {
+  if (!strcmp (method, "GET")) {return send_page(connection, "No GET allowed !!!\n", MHD_HTTP_OK);}
+  // Must be POST or PUT w. Body
+  
+  printf("URL(%s): %s Datasize: %lu, con_cls: %p\n", method, url, *upload_data_size, *con_cls);
+  
+  
   CONNINFO *con_info = NULL; // *con_cls;
   int pret = post_body_parse(connection, upload_data, upload_data_size, con_cls);
-  if (!pret) { return MHD_NO; }
+  if (!pret) { return MHD_NO; } // Fatal error - terminate request
   if (!*con_cls) { fprintf(stderr, "No *con_cls - should never happen!\n"); return MHD_NO; } // assert
   con_info = *con_cls; 
   if (con_info->is_parsing < 2) { return MHD_YES; }
@@ -249,7 +306,7 @@ int answer_to_connection0 (void *cls, struct MHD_Connection *connection,
 }
 
 void * req_term_cb(void *cls, struct MHD_Connection * connection, void **con_cls, enum MHD_RequestTerminationCode toe) {
-  fprintf(stderr, "Request Terminated (%p) !\n", *con_cls);
+  fprintf(stderr, "Request Terminated (con_cls=%p, cls=%p) !\n", *con_cls, cls);
   con_info_destroy((CONNINFO *) *con_cls);
   return NULL;
 }
