fce8675c19efc948546de2440dec71d5723bde58
Olli Hollmen <olli.hollmen@gmail.com>
2021-02-18 19:52:58 -0800

    Documentation, commnet and indentation changes. Rename proctest.c to proclist.c

--- Makefile
+++ Makefile
@@ -9,18 +9,18 @@ all: libs main
 
 libs:
 	# Actually test-exe
-	$(CC) -DTEST_MAIN -o proctest proctest.c -lprocps -ljansson
+	$(CC) -DTEST_MAIN -o proclist proclist.c -lprocps -ljansson
 	#  -lprocps
-	$(CC) -c proctest.c -o proctest.o
-	echo "Run test main: ./proctest"
+	$(CC) -c proclist.c -o proclist.o
+	echo "Run test main: ./proclist"
 	echo "Compile Process server: make main"
 main:
 	$(CC) -c procserver.c
 	#OLD:$(CC) -o procserver procserver.c proctest.o $(LIBS)
-	$(CC) -o procserver procserver.o proctest.o $(LIBS)
-	echo "Run by: ./procserver"
+	$(CC) -o procserver procserver.o proclist.o $(LIBS)
+	echo "Run by: ./procserver PORT (e.g. 8181)"
 clean:
-	rm -f procserver proctest ulftest proctest *.o
+	rm -f procserver proclist ulftest *.o
 test:
 	# -d "data"
 	curl -X POST -H "Content-Type: application/json" --data-binary @test.json --output - http://localhost:8001/json
@@ -30,3 +30,6 @@ ulf:
 	$(CC) $(CFLAGS) ulftest.c -DDEBUG -g -O0
 	# $(LIBS)  -lorcania
 	$(CC) -o ulftest ulftest.o -lc -lulfius
+docs:
+	mkdir doxydocs
+	doxygen doxy.conf
--- proclist.c
+++ proclist.c
@@ -1,14 +1,28 @@
 /**
-* Depends on libpropcs-dev
+* 
+* Depends on (Ubuntu/Debian) libpropcs-dev
 * g++ -o proctest proctest.c -lprocps
 * gcc -o proctest proctest.c -lprocps
 * Running
 * ./proctest > procs.json
-# Testing
-* python -m json.tool procs.json
-* # Alternatives to printf-JSON
+* 
+* See proc_t structure def in proc/readproc.h (/usr/include/proc/readproc.h)
+* ```
+* find /usr/include/ -name readproc.h | xargs -n 1 less -N
+* # Very likely the /usr/include/proc/readproc.h
+* ```
 
-General: https://stackoverflow.com/questions/6673936/parsing-json-using-c
+* # Testing
+* ./proctest | python -m json.tool
+* 
+* # Naming of process members in JSON
+* 
+* Naming comes from
+* - readproc.h (struct proc_t)
+* - 
+* # Alternatives to printf-JSON
+*
+* General: https://stackoverflow.com/questions/6673936/parsing-json-using-c
 
 * - libjson-c3 (json-c) - Github https://github.com/json-c/json-c, APT: libjson-c3 and libjson-c-dev
 * - libfastjson4 - fork of json-c (APT: libfastjson4, libfastjson-dev)
@@ -17,19 +31,31 @@ General: https://stackoverflow.com/questions/6673936/parsing-json-using-c
 * - cJSON - https://github.com/DaveGamble/cJSON
 * - nxJSON - https://bitbucket.org/yarosla/nxjson (Uses in-place strings)
 * - Frozen - https://github.com/cesanta/frozen (Sergey Lyubka)
+*
 * # Info / References
-https://stackoverflow.com/questions/49437192/libprocps-readproc-stack-smashing
-https://stackoverflow.com/questions/939778/linux-api-to-list-running-processes
-https://github.com/reklatsmasters/node-process-list
+*
+* - https://stackoverflow.com/questions/49437192/libprocps-readproc-stack-smashing
+* - https://stackoverflow.com/questions/939778/linux-api-to-list-running-processes
+* - https://github.com/reklatsmasters/node-process-list
+* - https://stackoverflow.com/questions/58885831/what-does-reaping-children-imply
+* - https://idea.popcount.org/2012-12-11-linux-process-states/
+* - man wait, main waitpid, man openproc, man readproc
+* - https://gitlab.com/procps-ng/procps
+* 
+* # Notes on ps STIME and procps start_time
+* https://gitlab.com/procps-ng/procps/-/blob/master/ps/output.c
+* Possibly do: time_t t = getbtime() + pp->start_time / Hertz;
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <proc/readproc.h> // /usr/include/proc/readproc.h
+#include <proc/sysinfo.h> // Hertz
 #include <string.h>
 
 #include <jansson.h>
 // #include <iostream>
-
+// kthreadd is pid 2, all kernel threads parent to it (ppid=2)
+#define IS_KTHREAD(proc)  (proc.ppid <= 2)
 // using namespace std;
 
 int arr2json(char ** arr, int cnt) {
@@ -42,14 +68,17 @@ int arr2json(char ** arr, int cnt) {
   
   fputs("]\n", stdout);
 }
+/* Default flass as
+NOT: PROC_FILLENV
+CONSIDER: PROC_UID (Creates empty list) PROC_FILLUSR
+*/
+int flags_def = PROC_FILLMEM | PROC_FILLSTAT | PROC_FILLSTATUS | PROC_FILLCOM | PROC_FILLUSR; // PROC_UID
 
-int flags_def = PROC_FILLMEM | PROC_FILLSTAT | PROC_FILLSTATUS | PROC_FILLCOM;
-
-/** Create a process list as JSON.
+/** Create a process list directly from readproc() listing as JSON.
 * For flags see FLAGS in man openproc.
 * @param flags The openproc() flags that define the extent of parsing process
 * info from /proc/$PID (There are ~ 20 flags available).
-* @return Process JSON string or NULL in case of errors (error messages get written to stderr).
+* @return Dynamically allocated process listing JSON string or NULL in case of errors (error messages get written to stderr).
 * @todo Make process attibutes more configurable. Possibly corralate fields to generate to flags passed as input (need lookup tables for those).
 * @todo Possibly wrap response with additional JSON object to be able to convey
 * status and error messages.
@@ -69,24 +98,27 @@ char * proc_list_json(int flags) {
   strncat(jsonstr, "[\n", 2); jpos += 2;
   
   // PROC_FILLUSR
-  if (!flags) { flags = flags_def; }
+  if (!flags) { flags = flags_def; } // Default flags
   PROCTAB * proclist = openproc(flags);
   if (!proclist) { fputs("openproc() Error\n", stderr); return NULL; }
   for (i=0 ; readproc(proclist, &proc) != NULL; i++) {
     // procstrbuf
+    // Kernel thread ppid == 0
+    if (IS_KTHREAD(proc)) { continue; }
+    jpos += sprintf(&(jsonstr[jpos]), "{\"cmd\":\"%s\", \"ppid\": %5d, \"rss\":%5ld", // "},\n",
+       proc.cmd, proc.ppid, proc.rss);
     
-    jpos += sprintf(&(jsonstr[jpos]), "{\"cmd\":\"%s\", \"ppid\": %5d, \"rss\":%5ld}" ",\n",
-      proc.cmd, proc.ppid, proc.rss);
-      // printf("Got ppid: %d\n", proc.ppid);
-      //arr[i] = strdup(procstrbuf); // Dup. individual JSON string
-      //if (!arr[i]) {} // Error
-      // See the need to realloc()
-      if ((jlen - jpos) < 10) {
-        int jlen2 = 2*jlen;
-        jsonstr = realloc(jsonstr, jlen2);
-	fprintf(stderr, "Triggered realloc(): %lu\n", (unsigned long)jsonstr);
-	jlen = jlen2;
-      } // (jpos / jlen) > 0.95
+    jpos += sprintf(&(jsonstr[jpos]), "},\n");
+    // printf("Got ppid: %d\n", proc.ppid);
+    //arr[i] = strdup(procstrbuf); // Dup. individual JSON string
+    //if (!arr[i]) {} // Error
+    // See the need to realloc()
+    if ((jlen - jpos) < 10) {
+      int jlen2 = 2*jlen;
+      jsonstr = realloc(jsonstr, jlen2);
+      fprintf(stderr, "Triggered realloc(): %lu\n", (unsigned long)jsonstr);
+      jlen = jlen2;
+    } // (jpos / jlen) > 0.95
   }
   closeproc(proclist);
   jpos -= 2; jsonstr[jpos] = '\0'; strncat(jsonstr, "\n]\n", 3); jpos += 3;
@@ -120,6 +152,8 @@ int list2str(char ** list, char buf[]) {
 /** Create Process list JSON.
 * Serialization is done using Jansson JSON library by building AoO and
 * serializing it.
+* @param flags - Flags for openproc() (See man openproc)
+* @return JSON array of (process) objects as jansson library data structure.
 */
 char * proc_list_json2(int flags) {
   proc_t proc; // Proc Instance
@@ -129,10 +163,13 @@ char * proc_list_json2(int flags) {
   PROCTAB * proclist = openproc(flags);
   if (!proclist) { fputs("openproc() Error\n", stderr); return NULL; }
   int i;
-  // THings to be refined
-  char starr[2] = {0, 0};
-  char cmdline[2048] = {0}; //  stack smashing detected
+  printf("Hertz=%Ld\n", Hertz);
+  // Things to be refined
+  char starr[2] = {0, 0}; // Single letter process state
+  char cmdline[2048] = {0}; //  stack smashing detected (re-terminate before populating each time)
   for (i=0 ; readproc(proclist, &proc) != NULL; i++) {
+    if (IS_KTHREAD(proc)) { continue; }
+    //json_t * proc_to_json(proc_t * proc) {
     json_t *obj = json_object();
     json_object_set(obj, "pid", json_integer(proc.tid));
     json_object_set(obj, "cmd",  json_string(proc.cmd)); // bn of executable
@@ -143,18 +180,24 @@ char * proc_list_json2(int flags) {
     if (ok) { json_object_set(obj, "cmdline",  json_string(cmdline)); }
     json_object_set(obj, "ppid", json_integer(proc.ppid));
     json_object_set(obj, "rss",  json_integer(proc.rss));
-    // Times start_time
+    // Times: start_time (should be seconds after 1970-1-1, but rolls on!)
     json_object_set(obj, "starttime", json_integer(proc.start_time));
+    
     json_object_set(obj, "utime", json_integer(proc.utime));
     json_object_set(obj, "stime", json_integer(proc.stime));
-    // User info ruser[P_G_SZ]
+    // Also cutime, cstime for cumulated
+    //json_object_set(obj, "utime", json_integer(proc.cutime));
+    //json_object_set(obj, "stime", json_integer(proc.cstime));
+    // User info ruser[P_G_SZ] - Only populated w. flag PROC_FILLUSR
     json_object_set(obj, "owner",  json_string(proc.ruser));
     
     // State: CPU/ processor, Process state (S=sleeping)
     json_object_set(obj, "cpuid", json_integer(proc.processor));
-    //state,
+    //state, R=Running (or runnable), S=sleeping (Interruptable sleep), Z=Defunct/Zombie, D=Uninterruptable sleep (usually IO)
+    // T: Stopped (e.g. ctrl-Z), I=???(On kernel threads)
     starr[0] = proc.state;
     json_object_set(obj, "state",  json_string(starr));
+    //}
     // See also: json_array_append_new(array, obj); // Steals value ...
     json_array_append(array, obj);
   }
@@ -162,6 +205,7 @@ char * proc_list_json2(int flags) {
   size_t jflags = JSON_COMPACT; // JSON_INDENT(2); // JSON_COMPACT; // JSON_ENSURE_ASCII
   // json_dumpfd()
   char * json = json_dumps(array, jflags);
+  // Need to free array and obj:s within it ?
   return json;
 }
 /** Tester utility for process JSON generation.
