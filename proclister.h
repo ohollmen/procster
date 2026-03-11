78185796ec0a48a37241728326f3d80d0571d4f1
Olli Hollmen <olli.hollmen@gmail.com>
2026-03-11 11:20:19 -0700

    Differentiate (old) libprocps vs. (new) libproc2 (2025-07-14)

--- proclister.h
+++ proclister.h
@@ -1,7 +1,15 @@
 #include <glib.h>
 #define WITH_SYSTEMD
+#ifdef PROC2
+#include <libproc2/pids.h>
+// Only essentials
+struct pids_fetch * proc_fetch(struct pids_info ** info);
+json_t * proc2_list_json(struct pids_info *info, struct pids_fetch * fetch_result);
+json_t * proc2_tree(struct pids_info *info, struct pids_fetch * fetch_result);
+#else
 #include <proc/readproc.h>
 #include <proc/sysinfo.h> // Hertz, getbtime
+
 
 // Need PROC_FILLSTATUS for ppid, need PROC_FILLSTAT for times.
 #define PROC_FLAGS_DEFAULT PROC_FILLMEM | PROC_FILLCOM | PROC_FILLUSR | PROC_FILLSTATUS | PROC_FILLSTAT
@@ -25,9 +33,15 @@ char * proc_list_json(int flags);
 // New return-JSON way ...
 json_t * proc_list_json2(int flags);
 void ptree_free(proc_t * p, int lvl);
+#endif
+
+
+// Neutal / process-lib agnostic
 int proc_kill(int pid);
 int pid_extract(const char * url);
 
+// Redudant, unused ?
+/*
 typedef struct pstree {
   GHashTable * idx; // Index for fast lookup
   PROCTAB * proctab; // Handle to opened procps "process table"
@@ -35,3 +49,4 @@ typedef struct pstree {
   GSList * list; // For temporarily unresolved parents
   int debug; // Debugging on
 } pstree;
+*/
