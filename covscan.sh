f7e482df6917e8b92ed8d4e7d8960d5ae54dd21e
Olli Hollmen <olli.hollmen@gmail.com>
2026-03-31 22:20:20 -0700

    Move model-file translation towards end as the cov-make-library binary is missing from the scan.coverity.com coverity distro. Fix intermediate dir name to please scan.coverity.com conventions.

--- covscan.sh
+++ covscan.sh
@@ -4,27 +4,35 @@
 # https://community.synopsys.com/s/article/Is-it-possible-to-get-the-issue-categorization-e-g-high-impact-medium-by-running-cov-analyze-only
 # https://community.synopsys.com/s/article/Coverity-PW-checker
 # COVINSTPATH=/usr/local/cov_2020.09/Linux-64/
+# Note: Converted to use intermediate directory name "cov-int" (not covint) as https://scan.coverity.com
+# seems to be strict about this.
 if [ -z $COVINSTPATH ]; then
   echo "No COVINSTPATH (Coverity installation path) given !"; exit 1
 fi
 export PATH="$COVINSTPATH/bin/:$PATH"
 CHECK_ENADISA=
-MODELFILE=covint/covmodels.xmldb
+MODELFILE=cov-int/covmodels.xmldb
 PW_CFG=
 # --disable CHECKED_RETURN
-mkdir covint
-# cp $COVINSTPATH/config/parse_warnings.conf.sample ./covint/parse_warnings.conf
-echo "chk \"PW.ASSIGN_WHERE_COMPARE_MEANT\": off;" > ./covint/parse_warnings.conf
-# Compile model
-cov-make-library --output-file covint/covmodels.xmldb covmodels.c
-cov-configure --config covint/coverity_config.xml --gcc
+mkdir -p cov-int
+# cp $COVINSTPATH/config/parse_warnings.conf.sample ./cov-int/parse_warnings.conf
+echo "chk \"PW.ASSIGN_WHERE_COMPARE_MEANT\": off;" > ./cov-int/parse_warnings.conf
 
-cov-build --dir covint --config covint/coverity_config.xml make
+# Also (e.g.): cov-configure --comptype gcc --compiler /usr/bin/mygcc-4.6
+cov-configure --config cov-int/coverity_config.xml --gcc
+# Run Build (check tail cov-int/build-log.txt after)
+cov-build --dir cov-int --config cov-int/coverity_config.xml make all2
+# cov-manage-emit is still included in the scan.coverity.com version.
 # cov-manage-emit ...
 # --checker-option / -co
+command -v cov-analyze >/dev/null 2>&1 || { echo "Utility cov-analyze missing (probably using Coverity Scan build-only package)"; exit 0; }
+# Both cov-analyze and cov-format-errors missing from scan.coverity.com
+# downloadable version => upload tar.gz:ed cov-int to scan.coverity.com.
+# Compile model (covmodels.c => conf/covmodels.c)
+# Even cov-make-library is missing in scan.coverity.com version !!!
+cov-make-library --output-file cov-int/covmodels.xmldb conf/covmodels.c
 # Use model:
-MOD="--user-model-file covint/covmodels.xmldb"
+MOD="--user-model-file cov-int/covmodels.xmldb"
 # --disable PW.ASSIGN_WHERE_COMPARE_MEANT
-cov-analyze --dir covint --strip-path `pwd`  $MOD --aggressiveness-level high --enable-parse-warnings --parse-warnings-config ./covint/parse_warnings.conf --disable DEADCODE
-cov-format-errors --dir covint --json-output-v7 covint/errors.json
-
+cov-analyze --dir cov-int --strip-path `pwd`  $MOD --aggressiveness-level high --enable-parse-warnings --parse-warnings-config ./cov-int/parse_warnings.conf --disable DEADCODE
+cov-format-errors --dir cov-int --json-output-v7 cov-int/errors.json
